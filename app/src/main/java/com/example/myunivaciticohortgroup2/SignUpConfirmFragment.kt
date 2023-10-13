bf40b848cd6ec0611941973e3bc56131a01edee0
Janviertyga <janviertyga@gmail.com>
2023-10-13 09:01:23 +0200

    Android layout

---
+++ app/src/main/java/com/example/myunivaciticohortgroup2/SignUpConfirmFragment.kt
@@ -0,0 +1,52 @@
+package com.example.myunivaciticohortgroup2
+
+import android.R
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SignUpConfirmFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+
+
+class SignUpConfirmFragment: Activity() {
+    private var _bg__sign_up_ek2: View? = null
+    private var __img___freebg_sign_up: ImageView? = null
+    private var _bg__battery_half_ek1: View? = null
+    private var vector: ImageView? = null
+    private var _bg__signal_ek1: View? = null
+    private var vector_ek1: ImageView? = null
+    private var vector_ek2: ImageView? = null
+    private var _09_41: TextView? = null
+    private var _null_: TextView? = null
+    private var set_password: TextView? = null
+    private var _bg__frame_2_ek1: View? = null
+    private var minimum_of_6_characters: TextView? = null
+    private var rectangle_13: View? = null
+    private var rectangle_14: View? = null
+    private var rectangle_15: View? = null
+    private var proceed: TextView? = null
+    private var password: TextView? = null
+    private var arrow_1: ImageView? = null
+    private var line_3: ImageView? = null
+    private var line_8: ImageView? = null
+    private var line_4: ImageView? = null
+    private var line_5: ImageView? = null
+    private var line_6: ImageView? = null
+    private var line_7: ImageView? = null
+    private var confirm_password: TextView? = null
+
+}
+
